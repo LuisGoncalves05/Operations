b28e898e2be3e97936e7640f322718dd9e2d902d
LuisGoncalves05 <lumicosgon.lg@gmail.com>
2024-09-30 09:55:57 +0100

    Operation implementation

--- operations/src/Main.java
+++ operations/src/Main.java
@@ -1,9 +1,14 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        String operation = scanner.nextLine();
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        System.out.println(a + b);
+        if (operation.equals("sum")) {
+            System.out.println(a + b);
+        } else {
+            System.out.println(a * b);
+        }
     }
 }
